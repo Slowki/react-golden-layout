aa6891c3a213a95d3680a5c936ab35fdf8669637
Stephan Wolski <stephan@wol.ski>
2018-03-18 21:56:41 -0400

    feat(stack): added header related properties to Stack

--- src/Stack.js
+++ src/Stack.js
@@ -6,10 +6,49 @@ import React from 'react';
 import type { ElementProps } from 'react';
 
 /**
+ * Properties for the `Stack` element
+ */
+export type StackProps = {|
+    children: ChildrenArray<any>;
+    itemRef?: (Object) => mixed;
+    headerRef?: (Object) => mixed;
+    headerPosition?: false | 'top' | 'left' | 'right' | 'bottom';
+    showPopoutIcon?: boolean;
+    showMaximiseIcon?: boolean;
+    showCloseIcon?: boolean;
+|};
+
+function showHideIcon(header : any, name : string, value : boolean) {
+    const icon = header.controlsContainer.find(`.lm_${name}`);
+    value ? icon.show() : icon.hide();
+}
+
+// TODO Stack should be a class with a willReceiveProps function
+/**
  * Create a new Stack
  */
-export default function Stack(props : ContentItemProps) : * {
-    return <ContentItem type="stack" {...props} />;
+export default function Stack(props : StackProps) : * {
+    function setSettings(item) {
+        if (props.headerPosition !== undefined)
+            item.header.position(props.headerPosition);
+
+        if (props.showPopoutIcon !== undefined) {
+            showHideIcon(item.header, 'popout', props.showMaximiseIcon);
+        }
+        
+        if (props.showMaximiseIcon !== undefined) {
+            showHideIcon(item.header, 'maximise', props.showMaximiseIcon);
+        }
+
+        if (props.showCloseIcon !== undefined) {
+            showHideIcon(item.header, 'close', props.showMaximiseIcon);
+        }
+
+        if (props.itemRef)
+            props.itemRef(item);
+    }
+
+    return <ContentItem itemRef={setSettings} type="stack" children={props.children} />;
 }
 
 markAsGLComponent(Stack);
