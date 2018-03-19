c209859ca277aad7e0109f24980ec1ee0c952619
Stephan Wolski <stephan@wol.ski>
2018-03-18 23:05:06 -0400

    refactor(types): moved shared type alias to their own file

---
+++ src/Types.js
@@ -0,0 +1,26 @@
+// @flow
+
+/**
+ * A GoldenLayout Item, see [GoldenLayout's documentation](http://golden-layout.com/docs/Item.html).
+ */
+export type GoldenLayoutItem = Object;
+
+/**
+ * A GoldenLayout Header object, see [GoldenLayout's documentation](http://golden-layout.com/docs/Header.html).
+ */
+export type GoldenLayoutHeader = Object;
+
+/**
+ * A GoldenLayout instance, see [GoldenLayout's documentation](https://golden-layout.com/docs/GoldenLayout.html).
+ * @external GoldenLayout 
+ */
+
+/**
+ * A [React component](https://reactjs.org/docs/react-api.html#reactcomponent)
+ * @external React.Component 
+ */
+
+/**
+ * A [React component](https://reactjs.org/docs/react-api.html#reactpurecomponent)
+ * @external React.PureComponent 
+ */
